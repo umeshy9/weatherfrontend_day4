007cd04d775b7f1010e6811b0c1455e265453dee
umeshy9120 <umesh.kumar@hashedin.com>
2020-12-23 15:12:23 +0530

    added styles div horizontal

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import WeatherFetch from './weatherFetch';
 const styles = {
   color: 'purple',
   fontSize: 10,
-  border: "1px solid purple",
+  border: "1px solid purple"
 };
 
 function App() {
   const api_key = process.env.REACT_APP_API_KEY;
   return (
-    <div style = {styles} className='row' >
-        <h1>Weather App</h1>
-        <div><WeatherFetch city = {'Bangalore'}/></div>
-        <div><WeatherFetch city = {'Delhi'}/></div>
-        <div><WeatherFetch city = {'Mumbai'}/></div>
+    
+    <div className = 'row' style = {styles}>
+        <div style = {{margin : 10}}><h1>Weather App</h1></div>
+        <div className = 'block'><WeatherFetch city = {'Bangalore'}/></div>
+        <div className = 'block'><WeatherFetch city = {'Delhi'}/></div>
+        <div className = 'block'><WeatherFetch city = {'Mumbai'}/></div>
         <div><WeatherFetch city = {'Chennai'}/></div>
     </div>
   );
